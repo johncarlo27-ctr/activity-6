2122c24b7e7f10c6e950776242d76adef035e5c5
unknown <johncarloaltura705@gmail.com>
2024-09-15 17:01:52 +0800

    Initial commit with data Conversion.js

---
+++ dataConversion.js
@@ -0,0 +1,25 @@
+let price = "200";
+//using persint()
+let num1 = parseInt(price);
+console.log(num1);
+//using number()
+let num2 = Number(price);
+console.log(num2)
+//Using the +price;
+let num3 = +price;
+console.log(num3)
+
+//Convert a Number to String 
+let distance = 150;
+//Using .toString()
+let str1 = distance.toString()
+console.log(str1);
+//Using String()
+let str2 = String(distance);
+console.log(str2);
+
+//Convert a String to a Decimal Number
+let weight = "72.5";
+//Using parseFloat()
+let decimalWeight = parseFloat(weight);
+console.log(decimalWeight);
